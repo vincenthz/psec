7796a43ebf50a9a24b761cb419726a8d9df7ff07
Nicolas Rolland <nicolas.rolland@gmail.com>
2016-10-09 14:16:27 +0100

    using cabal versionrange

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,9 +1,9 @@
+{-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Lib
     ( Issue(..)
     , PackageName(..)
-    , VersionSpecifier(..)
     , Version(..)
     -- * API
     , readDb
@@ -16,20 +16,17 @@ import qualified Data.Text as T
 import           Data.Time.Calendar
 import           Data.Version
 import           Text.ParserCombinators.ReadP
-import           Distribution.Version
+import           Distribution.Version(VersionRange(..))
+import           Distribution.Package (PackageName(..),PackageIdentifier(..))
 import qualified Distribution.Text         as C (parse)
 import qualified Distribution.Compat.ReadP as C (readP_to_S)
 
 -- useful ?
 -- data Severity = Critical | Major | Minor
 
-newtype PackageName = PackageName String
-    deriving (Show, Eq, FromJSON, ToJSON)
+deriving instance FromJSON PackageName
+deriving instance ToJSON PackageName
 
-data VersionSpecifier =
-      VersionOne Version
-    | VersionRange (Maybe Version) (Maybe Version)
-    deriving (Show,Eq)
 
 data Issue = Issue
     { issuePackage      :: PackageName
@@ -39,6 +36,7 @@ data Issue = Issue
     , issueReportedDate :: Day
     }
     deriving (Show,Eq)
+
 instance FromJSON VersionRange where
     parseJSON (String t) =
         case C.readP_to_S C.parse $ T.unpack t of
@@ -48,12 +46,6 @@ instance FromJSON VersionRange where
             l           -> return $ fst $ last l
 
 
-instance FromJSON VersionSpecifier where
-    parseJSON (String t) =
-        case readP_to_S parseVersion $ T.unpack t of
-            []          -> fail ("version parsing failed empty: " ++ show t)
-            [(ver, "")] -> return $ VersionOne ver
-            l           -> return $ VersionOne $ fst $ last l
 
 data Db = Db [Issue]
     deriving (Show,Eq)
@@ -74,5 +66,10 @@ readDb :: IO (Either ParseException Db)
 readDb = decodeFileEither "issues.yaml"
 
 -- | Get all issues matching the package version
+queryDb :: Db -> PackageIdentifier -> [Issue]
+queryDb (Db db) (PackageIdentifier pkgName pkgVersion) =
+   filter (undefined) undefined
 
-queryDb db pkgs = undefined
+
+
+
